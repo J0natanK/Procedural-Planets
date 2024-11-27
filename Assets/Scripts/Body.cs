330c1f284cdcbd033e719544988c2f4b7d518a8e
Jonatan K <jonkaj20@gmail.com>
2024-11-27 19:33:20 +0100

    First commit

---
+++ Assets/Scripts/Body.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body : MonoBehaviour
+{
+	public float mass;
+	public Vector3 initialVelocity;
+	
+	Transform transform;
+	
+	public void UpdateVelocity(Vector3 acceleration)
+	{
+		
+	}
+	
+	public void UpdatePosition()
+	{
+		transform.po
+	}
+}
